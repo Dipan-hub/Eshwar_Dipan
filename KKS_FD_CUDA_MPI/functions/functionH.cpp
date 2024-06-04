b6002ece57600d319ef266fd71dfd50fc46d7fdf
Dipan-hub <97114647+Dipan-hub@users.noreply.github.com>
2024-06-05 00:20:40 +0530

    Update and rename functionH.cu to functionH.cpp

--- KKS_FD_CUDA_MPI/functions/functionH.cpp
+++ KKS_FD_CUDA_MPI/functions/functionH.cpp
@@ -1,71 +1,74 @@
-#include "functionH.cuh"
+#include "functionH.hpp"
 
-extern __device__
 double calcInterp3rd(double **phi, long a, long idx, long NUMPHASES)
 {
     if (NUMPHASES < 2)
         return 0.0;
 
     double phiValue = phi[a][idx];
-    double ans = 3.0*phiValue*phiValue - 2.0*phiValue*phiValue*phiValue;
+    double ans = 3.0 * phiValue * phiValue - 2.0 * phiValue * phiValue * phiValue;
 
     double sum1 = 0.0;
 
+    #pragma acc parallel loop reduction(+:sum1) present(phi[0:NUMPHASES][0:idx+1])
     for (long b = 0; b < NUMPHASES; b++)
     {
         for (long c = 0; c < NUMPHASES; c++)
         {
             if (b != a && c != a && b < c)
             {
-                sum1 += phi[b][idx]*phi[c][idx];
+                sum1 += phi[b][idx] * phi[c][idx];
             }
         }
     }
 
-    sum1 *= 2.0*phiValue;
+    sum1 *= 2.0 * phiValue;
 
     return ans + sum1;
 }
 
-extern __device__
 double calcInterp3rdDiff(double **phi, long a, long b, long idx, long NUMPHASES)
 {
     long e, f;
     double sum = 0.0;
 
-  if (a == b)
-  {
-    sum = 6.0*phi[b][idx]*(1.0-phi[b][idx]);
-    for (e=0; e < NUMPHASES; e++) {
-      for (f=0; f < NUMPHASES; f++) {
-        if (e!=a && f!=a && e<f) {
-          sum += 2.0*phi[e][idx]*phi[f][idx];
+    if (a == b)
+    {
+        sum = 6.0 * phi[b][idx] * (1.0 - phi[b][idx]);
+        #pragma acc parallel loop reduction(+:sum) present(phi[0:NUMPHASES][0:idx+1])
+        for (e = 0; e < NUMPHASES; e++) {
+            for (f = 0; f < NUMPHASES; f++) {
+                if (e != a && f != a && e < f) {
+                    sum += 2.0 * phi[e][idx] * phi[f][idx];
+                }
+            }
         }
-      }
+    } 
+    else 
+    {
+        #pragma acc parallel loop reduction(+:sum) present(phi[0:NUMPHASES][0:idx+1])
+        for (e = 0; e < NUMPHASES; e++) {
+            if (e != b && e != a) {
+                sum += 2.0 * phi[e][idx];
+            }
+        }
+        sum *= phi[a][idx];
     }
-  } else {
-    for (e=0; e < NUMPHASES; e++) {
-      if (e!=b && e!=a) {
-        sum += 2.0*phi[e][idx];
-      }
-    }
-    sum *= phi[a][idx];
-  }
 
-  return sum;
+    return sum;
 }
 
-extern __device__
 double calcInterp5th(double **phi, long a, long idx, long NUMPHASES)
 {
     if (NUMPHASES < 2)
         return 0.0;
 
     double ans = 0.0, temp = 0.0;
-    double const1 = 7.5*((double)NUMPHASES-2.0)/((double)NUMPHASES-1.0);
+    double const1 = 7.5 * ((double)NUMPHASES - 2.0) / ((double)NUMPHASES - 1.0);
 
-    ans = phi[a][idx]*phi[a][idx]*phi[a][idx]*phi[a][idx]*(phi[a][idx]*(6.0 - const1) - 15.0 + 3.0*const1);
+    ans = phi[a][idx] * phi[a][idx] * phi[a][idx] * phi[a][idx] * (phi[a][idx] * (6.0 - const1) - 15.0 + 3.0 * const1);
 
+    #pragma acc parallel loop reduction(+:temp) present(phi[0:NUMPHASES][0:idx+1])
     for (long i = 1; i < NUMPHASES; i++)
     {
         if (i != a)
@@ -74,22 +77,23 @@ double calcInterp5th(double **phi, long a, long idx, long NUMPHASES)
             {
                 if (j != a)
                 {
-                    temp += (phi[j][idx] - phi[i][idx])*(phi[j][idx] - phi[i][idx]);
+                    temp += (phi[j][idx] - phi[i][idx]) * (phi[j][idx] - phi[i][idx]);
                 }
             }
         }
     }
-    temp *= 7.5*(phi[a][idx] - 1.0)/((double)NUMPHASES-1.0);
-    temp += phi[a][idx]*(10.0 - 3.0*const1) + const1;
-    ans  += phi[a][idx]*phi[a][idx]*temp;
+    temp *= 7.5 * (phi[a][idx] - 1.0) / ((double)NUMPHASES - 1.0);
+    temp += phi[a][idx] * (10.0 - 3.0 * const1) + const1;
+    ans += phi[a][idx] * phi[a][idx] * temp;
 
     if (NUMPHASES == 2)
         return ans;
 
-    temp  = 0.0;
+    temp = 0.0;
 
     if (NUMPHASES > 3)
     {
+        #pragma acc parallel loop reduction(+:temp) present(phi[0:NUMPHASES][0:idx+1])
         for (long i = 2; i < NUMPHASES; i++)
         {
             if (i != a)
@@ -102,19 +106,20 @@ double calcInterp5th(double **phi, long a, long idx, long NUMPHASES)
                         {
                             if (k != a)
                             {
-                                temp += phi[i][idx]*phi[j][idx]*phi[k][idx];
+                                temp += phi[i][idx] * phi[j][idx] * phi[k][idx];
                             }
                         }
                     }
                 }
             }
         }
-        ans += 15.0*phi[a][idx]*phi[a][idx]*temp;
+        ans += 15.0 * phi[a][idx] * phi[a][idx] * temp;
         temp = 0.0;
     }
 
     if (NUMPHASES > 4)
     {
+        #pragma acc parallel loop reduction(+:temp) present(phi[0:NUMPHASES][0:idx+1])
         for (long i = 3; i < NUMPHASES; i++)
         {
             if (i != a)
@@ -131,7 +136,7 @@ double calcInterp5th(double **phi, long a, long idx, long NUMPHASES)
                                 {
                                     if (l != a)
                                     {
-                                        temp += phi[i][idx]*phi[j][idx]*phi[k][idx]*phi[l][idx];
+                                        temp += phi[i][idx] * phi[j][idx] * phi[k][idx] * phi[l][idx];
                                     }
                                 }
                             }
@@ -140,13 +145,15 @@ double calcInterp5th(double **phi, long a, long idx, long NUMPHASES)
                 }
             }
         }
-        ans += 24.0*phi[a][idx]*temp;
+        ans += 24.0 * phi[a][idx] * temp;
     }
 
     return ans;
 }
 
-extern __device__
+//2nd
+
+
 double calcInterp5thDiff(double **phi, long a, long b, long idx, long NUMPHASES)
 {
     if (NUMPHASES < 2)
@@ -156,12 +163,13 @@ double calcInterp5thDiff(double **phi, long a, long b, long idx, long NUMPHASES)
     double temp = 0.0;
 
     double phiValue = phi[a][idx];
-    double const1 = 7.5*((double)NUMPHASES-2.0)/((double)NUMPHASES-1.0);
+    double const1 = 7.5 * ((double)NUMPHASES - 2.0) / ((double)NUMPHASES - 1.0);
 
     if (a == b)
     {
-        ans = phiValue*phiValue*phiValue*(5.0*phiValue*(6.0 - const1) + 4.0*(3.0*const1 - 15.0));
+        ans = phiValue * phiValue * phiValue * (5.0 * phiValue * (6.0 - const1) + 4.0 * (3.0 * const1 - 15.0));
 
+        #pragma acc parallel loop reduction(+:temp) present(phi[0:NUMPHASES][0:idx+1])
         for (long i = 1; i < NUMPHASES; i++)
         {
             if (i != a)
@@ -170,22 +178,23 @@ double calcInterp5thDiff(double **phi, long a, long b, long idx, long NUMPHASES)
                 {
                     if (j != a)
                     {
-                        temp += (phi[j][idx] - phi[i][idx])*(phi[j][idx] - phi[i][idx]);
+                        temp += (phi[j][idx] - phi[i][idx]) * (phi[j][idx] - phi[i][idx]);
                     }
                 }
             }
         }
 
-        temp *= 7.5*(3.0*phiValue - 2.0)/((double)NUMPHASES-1.0);
-        temp += 3.0*phiValue*(10.0 - 3.0*const1) + 2.0*const1;
-        ans  += phiValue*temp;
+        temp *= 7.5 * (3.0 * phiValue - 2.0) / ((double)NUMPHASES - 1.0);
+        temp += 3.0 * phiValue * (10.0 - 3.0 * const1) + 2.0 * const1;
+        ans += phiValue * temp;
 
         if (NUMPHASES == 2)
             return ans;
 
-        temp  = 0.0;
+        temp = 0.0;
         if (NUMPHASES > 3)
         {
+            #pragma acc parallel loop reduction(+:temp) present(phi[0:NUMPHASES][0:idx+1])
             for (long i = 2; i < NUMPHASES; i++)
             {
                 if (i != a)
@@ -198,19 +207,20 @@ double calcInterp5thDiff(double **phi, long a, long b, long idx, long NUMPHASES)
                             {
                                 if (k != a)
                                 {
-                                    temp += phi[i][idx]*phi[j][idx]*phi[k][idx];
+                                    temp += phi[i][idx] * phi[j][idx] * phi[k][idx];
                                 }
                             }
                         }
                     }
                 }
             }
-            ans += 30.0*phiValue*temp;
+            ans += 30.0 * phiValue * temp;
             temp = 0.0;
         }
 
         if (NUMPHASES > 4)
         {
+            #pragma acc parallel loop reduction(+:temp) present(phi[0:NUMPHASES][0:idx+1])
             for (long i = 3; i < NUMPHASES; i++)
             {
                 if (i != a)
@@ -227,7 +237,7 @@ double calcInterp5thDiff(double **phi, long a, long b, long idx, long NUMPHASES)
                                     {
                                         if (l != a)
                                         {
-                                            temp += phi[i][idx]*phi[j][idx]*phi[k][idx]*phi[l][idx];
+                                            temp += phi[i][idx] * phi[j][idx] * phi[k][idx] * phi[l][idx];
                                         }
                                     }
                                 }
@@ -236,7 +246,7 @@ double calcInterp5thDiff(double **phi, long a, long b, long idx, long NUMPHASES)
                     }
                 }
             }
-            ans += 24.0*temp;
+            ans += 24.0 * temp;
         }
     }
     else
@@ -244,12 +254,13 @@ double calcInterp5thDiff(double **phi, long a, long b, long idx, long NUMPHASES)
         if (NUMPHASES == 2)
         {
             //ans = 0.0;
-            ans = -30.0*phiValue*phiValue*(1.0-phiValue)*(1.0-phiValue);
+            ans = -30.0 * phiValue * phiValue * (1.0 - phiValue) * (1.0 - phiValue);
             return ans;
         }
 
         if (NUMPHASES > 2)
         {
+            #pragma acc parallel loop reduction(+:temp) present(phi[0:NUMPHASES][0:idx+1])
             for (long i = 1; i < NUMPHASES; i++)
             {
                 if (i != a)
@@ -259,18 +270,19 @@ double calcInterp5thDiff(double **phi, long a, long b, long idx, long NUMPHASES)
                         if (j != a)
                         {
                             if (i == b)
-                                temp += -2.0*(phi[j][idx] - phi[i][idx]);
+                                temp += -2.0 * (phi[j][idx] - phi[i][idx]);
                             else if (j == b)
-                                temp += 2.0*(phi[j][idx] - phi[i][idx]);
+                                temp += 2.0 * (phi[j][idx] - phi[i][idx]);
                         }
                     }
                 }
             }
-            ans = (phiValue-1.0)*phiValue*phiValue*7.5*temp/((double)NUMPHASES-1.0);
+            ans = (phiValue - 1.0) * phiValue * phiValue * 7.5 * temp / ((double)NUMPHASES - 1.0);
             temp = 0.0;
         }
         if (NUMPHASES > 3)
         {
+            #pragma acc parallel loop reduction(+:temp) present(phi[0:NUMPHASES][0:idx+1])
             for (long i = 2; i < NUMPHASES; i++)
             {
                 if (i != a)
@@ -284,23 +296,24 @@ double calcInterp5thDiff(double **phi, long a, long b, long idx, long NUMPHASES)
                                 if (k != a)
                                 {
                                     if (i == b)
-                                        temp += phi[j][idx]*phi[k][idx];
+                                        temp += phi[j][idx] * phi[k][idx];
                                     else if (j == b)
-                                        temp += phi[i][idx]*phi[k][idx];
+                                        temp += phi[i][idx] * phi[k][idx];
                                     else if (k == b)
-                                        temp += phi[i][idx]*phi[j][idx];
+                                        temp += phi[i][idx] * phi[j][idx];
                                 }
                             }
                         }
                     }
                 }
             }
-            ans += 15*phiValue*phiValue*temp;
+            ans += 15 * phiValue * phiValue * temp;
             temp = 0.0;
         }
 
         if (NUMPHASES > 4)
         {
+            #pragma acc parallel loop reduction(+:temp) present(phi[0:NUMPHASES][0:idx+1])
             for (long i = 3; i < NUMPHASES; i++)
             {
                 if (i != a)
@@ -318,13 +331,13 @@ double calcInterp5thDiff(double **phi, long a, long b, long idx, long NUMPHASES)
                                         if (l != a)
                                         {
                                             if (i == b)
-                                                temp += phi[j][idx]*phi[k][idx]*phi[l][idx];
+                                                temp += phi[j][idx] * phi[k][idx] * phi[l][idx];
                                             else if (j == b)
-                                                temp += phi[i][idx]*phi[k][idx]*phi[l][idx];
+                                                temp += phi[i][idx] * phi[k][idx] * phi[l][idx];
                                             else if (k == b)
-                                                temp += phi[i][idx]*phi[j][idx]*phi[l][idx];
+                                                temp += phi[i][idx] * phi[j][idx] * phi[l][idx];
                                             else if (l == b)
-                                                temp += phi[i][idx]*phi[j][idx]*phi[k][idx];
+                                                temp += phi[i][idx] * phi[j][idx] * phi[k][idx];
                                         }
                                     }
                                 }
@@ -333,7 +346,7 @@ double calcInterp5thDiff(double **phi, long a, long b, long idx, long NUMPHASES)
                     }
                 }
             }
-            ans += 24.0*phiValue*temp;
+            ans += 24.0 * phiValue * temp;
         }
     }
 
