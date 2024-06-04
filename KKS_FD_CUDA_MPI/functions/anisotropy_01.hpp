a850b7c332038215c1fb6f5266d4e173e0bc9ba9
Dipan-hub <97114647+Dipan-hub@users.noreply.github.com>
2024-06-05 00:30:59 +0530

    Update and rename anisotropy_01.cuh to anisotropy_01.hpp

--- KKS_FD_CUDA_MPI/functions/anisotropy_01.hpp
+++ KKS_FD_CUDA_MPI/functions/anisotropy_01.hpp
@@ -1,14 +1,12 @@
-#ifndef ANISOTROPY_01_CUH_
-#define ANISOTROPY_01_CUH_
+#ifndef ANISOTROPY_01_HPP_
+#define ANISOTROPY_01_HPP_
 
 #include <stdio.h>
-#include <cuda.h>
-#include <cuda_runtime.h>
 
-extern __device__
+// Function prototypes for anisotropy operations
+
 void anisotropy_01_dAdq(double *qab, double *dadq, long a, long b, double *dab, long NUMPHASES);
 
-extern __device__
 double anisotropy_01_function_ac(double *qab, long a, long b, double *dab, long NUMPHASES);
 
-#endif
+#endif // ANISOTROPY_01_H_
