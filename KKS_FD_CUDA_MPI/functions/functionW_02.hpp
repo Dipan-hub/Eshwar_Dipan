f9e4208221b849224d01d76704f5a4f9fea2c960
Dipan-hub <97114647+Dipan-hub@users.noreply.github.com>
2024-06-05 00:18:20 +0530

    Update and rename functionW_02.cuh to functionW_02.hpp

--- KKS_FD_CUDA_MPI/functions/functionW_02.hpp
+++ KKS_FD_CUDA_MPI/functions/functionW_02.hpp
@@ -1,17 +1,13 @@
-#ifndef FUNCTIONW_02_CUH_
-#define FUNCTIONW_02_CUH_
+#ifndef FUNCTIONW_02_HPP_
+#define FUNCTIONW_02_HPP_
 
-#include <cuda.h>
-#include <cuda_runtime.h>
 #include <stdio.h>
 
 /*
- * __device__ double calcDoubleWellDerivative
- *
  * Calculate g'(\phi)
  *
  * Arguments:
- *              1. cufftDoubleComplex **phi -> all the phase volume-fraction values
+ *              1. double **phi -> all the phase volume-fraction values
  *              2. long phase -> differentiate wrt to this phase
  *              3. double *theta_i   -> coefficients for theta_i, one per phase
  *              4. double *theta_ij  -> coefficients for theta_ij, one per pair of phases
@@ -21,9 +17,8 @@
  * Return:
  *              numerical evaluation of derivative of interpolation polynomial, as a double datatype
  */
-extern __device__
 double calcDoubleWellDerivative(double **phi, long phase,
                                 double *theta_i, double *theta_ij, double *theta_ijk,
                                 long idx, long NUMPHASES);
 
-#endif
+#endif // FUNCTIONW_02_H_
