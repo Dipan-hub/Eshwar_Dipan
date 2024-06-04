e46c85d570ed21e23271bc58af95de23b430ef25
Dipan-hub <97114647+Dipan-hub@users.noreply.github.com>
2024-06-05 00:15:06 +0530

    Update and rename matrix.cuh to matrix.hpp

--- KKS_FD_CUDA_MPI/functions/matrix.hpp
+++ KKS_FD_CUDA_MPI/functions/matrix.hpp
@@ -1,25 +1,29 @@
-#ifndef MATRIX_CUH_
-#define MATRIX_CUH_
+#ifndef MATRIX_HPP_
+#define MATRIX_HPP_
 
-#include <cuda.h>
-#include <cuda_runtime.h>
-#include <stdio.h>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
 
-#include "structures.h"
+#include "structures.hpp"
 #include "utilityFunctions.h"
 
+// Define constants for maximum components
+#define MAX_NUM_COMP 100
+#define MAX_NUM_PHASE_COMP 100
+
 /*
- * Matrix operations, taken from the MicroSim GP solver's matrix.h
+ * Matrix operations
  */
-void matinvnew(double **coeffmatrix,double **inv, long size);
-void multiply(double **inv,double *y,double *prod, long size);
-void multiply2d(double **m1,double **m2,double **prod, long size);
-void vectorsum(double *y1,double *y2, double *sum, long size);
-void substituteb(double **fac,double *y,double *vec, long size);
-void substitutef(double **fac,double **y1,int index,double *vec, long size);
-void pivot(double **coeffmatrix,double **factor,int k,int *tag, long size);
-void colswap(double **m1,double **m2,int *tag, long size);
-void rowswap(double **m1,double **m2,int *tag, long size);
+void matinvnew(double **coeffmatrix, double **inv, long size);
+void multiply(double **inv, double *y, double *prod, long size);
+void multiply2d(double **m1, double **m2, double **prod, long size);
+void vectorsum(double *y1, double *y2, double *sum, long size);
+void substituteb(double **fac, double *y, double *vec, long size);
+void substitutef(double **fac, double **y1, int index, double *vec, long size);
+void pivot(double **coeffmatrix, double **factor, int k, int *tag, long size);
+void colswap(double **m1, double **m2, int *tag, long size);
+void rowswap(double **m1, double **m2, int *tag, long size);
 
 /*
  *  The following are host-side functions
@@ -29,45 +33,20 @@ void LUPInvert(double **A, int *P, int N, double **IA);
 void matrixMultiply(double **A, double **B, double **C, int N);
 
 /*
- *  The following are device-side functions
+ *  The following are previously device-side functions, now converted to standard C++ functions
  */
-extern __device__
 void multiply(double *A, double *B, double *C, long ip1, long ip2, long NUMPHASES, int N);
-
-extern __device__
 int LUPDecomposeC1(double A[][MAX_NUM_COMP], long N, double Tol, int *P);
-
-extern __device__
 int LUPDecomposeC2(double A[(MAX_NUM_COMP)*(MAX_NUM_COMP)], long N, double Tol, int *P);
-
-extern __device__
 void LUPSolveC1(double A[][MAX_NUM_COMP], int *P, double *b, long N, double *x);
-
-extern __device__
 void LUPSolveC2(double A[(MAX_NUM_COMP)*(MAX_NUM_COMP)], int *P, double *b, long N, double *x);
-
-extern __device__
 void LUPInvertC1(double A[][MAX_NUM_COMP], int *P, long N, double IA[][MAX_NUM_COMP]);
-
-extern __device__
 void LUPInvertC2(double A[(MAX_NUM_COMP)*(MAX_NUM_COMP)], int *P, long N, double IA[][MAX_NUM_COMP]);
-
-extern __device__
 int LUPDecomposePC1(double A[][MAX_NUM_PHASE_COMP], long N, double Tol, int *P);
-
-extern __device__
 int LUPDecomposePC2(double A[(MAX_NUM_PHASE_COMP)*(MAX_NUM_PHASE_COMP)], long N, double Tol, int *P);
-
-extern __device__
 void LUPSolvePC1(double A[][MAX_NUM_PHASE_COMP], int *P, double *b, long N, double *x);
-
-extern __device__
 void LUPSolvePC2(double A[(MAX_NUM_PHASE_COMP)*(MAX_NUM_PHASE_COMP)], int *P, double *b, long N, double *x);
-
-extern __device__
 void LUPInvertPC1(double A[][MAX_NUM_PHASE_COMP], int *P, long N, double IA[][MAX_NUM_PHASE_COMP]);
-
-extern __device__
 void LUPInvertPC2(double A[(MAX_NUM_PHASE_COMP)*(MAX_NUM_PHASE_COMP)], int *P, long N, double IA[][MAX_NUM_PHASE_COMP]);
 
-#endif
+#endif // MATRIX_HPP_
