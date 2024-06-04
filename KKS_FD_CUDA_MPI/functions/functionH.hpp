eacef5969f5404bae5542e472d5986d279b785fd
Dipan-hub <97114647+Dipan-hub@users.noreply.github.com>
2024-06-05 00:20:17 +0530

    Update and rename functionH.cuh to functionH.hpp

--- KKS_FD_CUDA_MPI/functions/functionH.hpp
+++ KKS_FD_CUDA_MPI/functions/functionH.hpp
@@ -1,20 +1,11 @@
-#ifndef FUNCTIONH_CUH_
-#define FUNCTIONH_CUH_
+#ifndef FUNCTIONH_HPP_
+#define FUNCTIONH_HPP_
 
-#include <cuda.h>
-#include <cuda_runtime.h>
 #include <stdio.h>
 
-extern __device__
 double calcInterp5th(double **phi, long a, long idx, long NUMPHASES);
-
-extern __device__
 double calcInterp5thDiff(double **phi, long a, long b, long idx, long NUMPHASES);
-
-extern __device__
 double calcInterp3rd(double **phi, long a, long idx, long NUMPHASES);
-
-extern __device__
 double calcInterp3rdDiff(double **phi, long a, long b, long idx, long NUMPHASES);
 
-#endif
+#endif // FUNCTIONH_HPP_
