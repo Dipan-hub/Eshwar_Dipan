e70899e6ad8b949a469f197a39ebd6b2afdd7ece
Dipan-hub <97114647+Dipan-hub@users.noreply.github.com>
2024-06-05 21:23:29 +0530

    Update anisotropy_01.hpp

--- KKS_FD_CUDA_MPI/functions/anisotropy_01.hpp
+++ KKS_FD_CUDA_MPI/functions/anisotropy_01.hpp
@@ -1,12 +1,11 @@
 #ifndef ANISOTROPY_01_HPP_
 #define ANISOTROPY_01_HPP_
 
-#include <stdio.h>
-
-// Function prototypes for anisotropy operations
+#include <cmath>
+#include <cstdio>
 
 void anisotropy_01_dAdq(double *qab, double *dadq, long a, long b, double *dab, long NUMPHASES);
 
 double anisotropy_01_function_ac(double *qab, long a, long b, double *dab, long NUMPHASES);
 
-#endif // ANISOTROPY_01_H_
+#endif 
