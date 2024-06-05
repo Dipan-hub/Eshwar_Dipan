64a0e81b36cc61fe5da69974bd6c2e297bf73ab1
Dipan-hub <97114647+Dipan-hub@users.noreply.github.com>
2024-06-05 21:30:59 +0530

    Update updatePhi.cuh

--- KKS_FD_CUDA_MPI/solverloop/updatePhi.cuh
+++ KKS_FD_CUDA_MPI/solverloop/updatePhi.cuh
@@ -1,37 +1,34 @@
-#ifndef UPDATEPHI_CUH_
-#define UPDATEPHI_CUH_
+#ifndef UPDATEPHI_HPP_
+#define UPDATEPHI_HPP_
 
-#include <cuda.h>
-#include <cuda_runtime.h>
 #include <stdio.h>
 
-#include "helper_string.h"
-#include "helper_cuda.h"
+#include "helper_string.hpp"
+#include "helper_cuda.hpp"
 
-#include "structures.h"
-#include "Thermo.cuh"
-#include "utilityKernels.cuh"
-#include "matrix.cuh"
-#include "functionA_01.cuh"
-#include "functionA_02.cuh"
-#include "functionTau.cuh"
-#include "boundary.cuh"
+#include "structures.hpp"
+#include "Thermo.hpp"
+#include "utilityKernels.hpp"
+#include "matrix.hpp"
+#include "functionA_01.hpp"
+#include "functionA_02.hpp"
+#include "functionTau.hpp"
+#include "boundary.hpp"
 
 /*
- * Kernel that solves d(\phi_{i})/dt = -L/N \sum_{j=1, i \neq j}^{N} (df/dphi_{i} - df/dphi_[j})
+ * Function prototype for the kernel that solves d(\phi_{i})/dt = -L/N \sum_{j=1, i \neq j}^{N} (df/dphi_{i} - df/dphi_{j})
  */
-__global__
-void __updatePhi__(double **phi, double **dfdphi, double **phiNew,
-                   double *relaxCoeff, double *kappaPhi,
-                   double *dab, int FUNCTION_ANISOTROPY,
-                   long NUMPHASES, long NUMCOMPONENTS, long DIMENSION, long FUNCTION_F,
-                   long sizeX, long sizeY, long sizeZ,
-                   long xStep, long yStep, long padding,
-                   double DELTA_X, double DELTA_Y, double DELTA_Z,
-                   double DELTA_t);
+void updatePhi_kernel(double **phi, double **dfdphi, double **phiNew,
+                      double *relaxCoeff, double *kappaPhi,
+                      double *dab, int FUNCTION_ANISOTROPY,
+                      long NUMPHASES, long NUMCOMPONENTS, long DIMENSION, long FUNCTION_F,
+                      long sizeX, long sizeY, long sizeZ,
+                      long xStep, long yStep, long padding,
+                      double DELTA_X, double DELTA_Y, double DELTA_Z,
+                      double DELTA_t);
 
 /*
- * Host-side wrapper function for __updatePhi__
+ * Host-side wrapper function for updatePhi_kernel
  */
 #ifdef __cplusplus
 extern "C"
